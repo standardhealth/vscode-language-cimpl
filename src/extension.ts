b46f729c2db9fc68a3b1267dfb7098f23ac2869d
Mahalingam <kmahalingam@mitre.org>
2018-10-03 14:44:03 -0400

    Implement folder parsing and allow JS modules

--- src/extension.ts
+++ src/extension.ts
@@ -8,29 +8,17 @@ import {
 	commands, window, workspace, Selection, TextEditorEdit, ExtensionContext
 } from 'vscode';
 
-// import { importFromFilePath } from './parser';
+import { importFromFilePath } from './parser';
 
 export function activate(context: ExtensionContext) {
 
-	// context.subscriptions.push(commands.registerCommand('extension.parseFolders', () => {
-	// 	let parsedFiles: any = {};
-	// 	const regexp = /file:\/\/(.+)/;
-	// 	workspace.workspaceFolders.forEach((folder) => {
-	// 		console.log(folder);
-	// 		const folderPath = regexp.exec(folder.uri.toString())[1];
-	// 		const parsedFolder = importFromFilePath(folderPath);
-	// 		parsedFiles = Object.assign(parsedFolder, parsedFiles);
-	// 		console.log(parsedFiles);
-	// 	});
-	// }));
-
-	// Example command for text editing, kept for reference
-	// context.subscriptions.push(commands.registerCommand('extension.sayHello', () => {
-	// 	const editor = window.activeTextEditor;
-	// 	const position = editor.selection.active;
-
-    //     editor.edit((editBuilder : TextEditorEdit) => {
-	// 		editBuilder.insert(position, "Hello");
-	// 	});
-	// }));
+	context.subscriptions.push(commands.registerCommand('extension.parseFolders', () => {
+		let parsedFiles: any = {};
+		const regexp = /file:\/\/(.+)/;
+		workspace.workspaceFolders.forEach((folder) => {
+			const folderPath = regexp.exec(folder.uri.toString())[1];
+			const parsedFolder = importFromFilePath(folderPath);
+			parsedFiles = Object.assign(parsedFolder, parsedFiles);
+		});
+	}));
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,8 +1,8 @@
 import fs = require('fs');
 import path = require('path');
 import { FileStream, CommonTokenStream } from 'antlr4/index';
-const { SHRDataElementParser } = require('./parsers/SHRDataElementParser');
-const { SHRDataElementLexer } = require('./parsers/SHRDataElementLexer');
+const { SHRDataElementParser } = require('./parsers/SHRDataElementParser.js');
+const { SHRDataElementLexer } = require('./parsers/SHRDataElementLexer.js');
 
 export function importFromFilePath(filePath: string) {
   const filesByType = processPath(filePath);

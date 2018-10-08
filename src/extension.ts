4a79284405a42ae546164e9b2273b888f2a2963f
Mahalingam <kmahalingam@mitre.org>
2018-10-08 14:58:28 -0400

    Get active file, current element, and selected text

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@
 'use strict';
 
 import {
-	commands, window, workspace, Selection, TextEditorEdit, ExtensionContext
+	commands, window, workspace, Selection, Position, TextEditor, TextEditorEdit, TextLine, ExtensionContext
 } from 'vscode';
 
 import { importFromFilePath } from './parser';
@@ -15,10 +15,46 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(commands.registerCommand('extension.parseFolders', () => {
 		let parsedFiles: any = {};
 		const regexp = /file:\/\/(.+)/;
+		
+		const editor: TextEditor = window.activeTextEditor;
+		const selection: Selection = editor.selection;
+		const position: Position = selection.active;
+		let elementName: string;
+		let selectedText: string;
+
+		for (let i: number = position.line; i >= 0; i--) {
+			const currentLine: TextLine = editor.document.lineAt(i);
+			if (currentLine.text.includes("Element:") && !currentLine.text.includes("//")) {
+				const currentLineArray: string[] = currentLine.text.replace(/\t/g, " ").split(" ");
+				elementName = currentLineArray[currentLineArray.length - 1];
+				break;
+			}
+		}
+
+		if (!selection.isEmpty) {
+			selectedText = editor.document.getText(selection);
+			window.showInformationMessage(`Selected Text: ${selectedText}`);
+		} else {
+			window.showErrorMessage("No text selected.");
+		}
+
+		if (elementName) {
+			window.showInformationMessage(`Current Element: ${elementName}`);
+		} else {
+			window.showErrorMessage("Not editing Element.");
+		}
+
 		workspace.workspaceFolders.forEach((folder) => {
 			const folderPath = regexp.exec(folder.uri.toString())[1];
 			const parsedFolder = importFromFilePath(folderPath);
 			parsedFiles = Object.assign(parsedFolder, parsedFiles);
 		});
+
+		const parsedActiveFile = parsedFiles[editor.document.fileName];
+		if (parsedActiveFile) {
+			window.showInformationMessage(`File ${editor.document.fileName} parsed.`);
+		} else {
+			window.showErrorMessage(`Could not parse file ${editor.document.fileName}.`)
+		}
 	}));
 }

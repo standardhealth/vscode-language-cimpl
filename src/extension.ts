76944fe9394d6f8954da3ce4a072108a63efefcb
Mahalingam <kmahalingam@mitre.org>
2018-10-23 13:22:35 -0400

    Get current element's attributes

--- src/extension.ts
+++ src/extension.ts
@@ -96,4 +96,100 @@ export function activate(context: ExtensionContext) {
 			window.showInformationMessage("Definition not found in workspace.");
 		}
 	}));
+
+	context.subscriptions.push(commands.registerCommand('extension.getInheritedAttributes', () => {
+		let parsedFiles: any = {};
+		const regexp = /file:\/\/(.+)/;
+		
+		let editor: TextEditor = window.activeTextEditor;
+
+		if (!editor) {
+			window.showErrorMessage("No active editor.");
+			return;
+		}
+
+		if (!(workspace && workspace.workspaceFolders)) {
+			window.showErrorMessage("No files in workspace.");
+			return;
+		}
+
+		const selection: Selection = editor.selection;
+		let selectedText: string;
+
+		if (!selection.isEmpty) {
+			selectedText = editor.document.getText(selection);
+		} else {
+			window.showErrorMessage("No text selected.");
+			return;
+		}
+
+		workspace.workspaceFolders.forEach((folder) => {
+			const folderPath = regexp.exec(folder.uri.toString())[1];
+			const parsedFolder = importFromFilePath(folderPath);
+			parsedFiles = Object.assign(parsedFolder, parsedFiles);
+		});
+
+		for (const fileName in parsedFiles) {			
+			const dataDefs = parsedFiles[fileName].children.find((c) => {
+				return c.constructor.name === 'DataDefsContext';
+			});
+
+			if (!dataDefs.children || (dataDefs.children.length <= 0)) {
+				continue;
+			}
+
+			for (const dataDef of dataDefs.children) {
+				const def = dataDef.children.find((c) => {
+					return c.constructor.name === 'ElementDefContext'
+					|| c.constructor.name === 'EntryDefContext';
+				});
+
+				const header = def.children.find((c) => {
+					return c.constructor.name === 'ElementHeaderContext'
+					|| c.constructor.name === 'EntryHeaderContext';
+				});
+
+				const simpleName = header.children.find((c) => {
+					return c.constructor.name === 'SimpleNameContext';
+				});
+
+				if (simpleName && (simpleName.start.text === selectedText)) {
+					const values = def.children.find((c) => {
+						return c.constructor.name === 'ValuesContext';
+					});
+
+					const fields = values.children.filter((c) => {
+						return c.constructor.name === 'FieldContext';
+					});
+
+					let attributes = {};
+
+					for (const field of fields) {
+						const fieldType = field.children.find((c) => {
+							return c.constructor.name === 'FieldTypeContext';
+						});
+
+						const simpleOrFQName = fieldType.children.find((c) => {
+							return c.constructor.name === 'SimpleOrFQNameContext';
+						});
+
+						const attributeName = simpleOrFQName.children.find((c) => {
+							return c.constructor.name === 'SimpleNameContext';
+						});
+
+						const count = field.children.find((c) => {
+							return c.constructor.name === 'CountContext';
+						});
+
+						if (attributeName && attributeName.start.text && count && count.start.text && count.stop.text) {
+							attributes[attributeName.start.text] = `${count.start.text}..${count.stop.text}`;
+						}
+					}
+					
+					window.showQuickPick(Object.keys(attributes));
+					break;
+				}
+			};
+		}
+	}));
 }
